f56f605cbd3107a45c2ea670690d422a6ab477c9
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-04-07 18:48:27 +0300

    [skip ci] remove css

--- src/components/PendingSwapTimeline.tsx
+++ src/components/PendingSwapTimeline.tsx
@@ -1,14 +1,18 @@
 import React, { ReactElement } from "react"
 
+import {
+  TimelineConnector,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab"
 import { ReactComponent as ArrowDownIcon } from "../assets/icons/arrowDown.svg"
-import { Box } from "@mui/material"
 import { ReactComponent as CheckIcon } from "../assets/icons/check.svg"
 import { ReactComponent as ClockIcon } from "../assets/icons/clock.svg"
 import { PendingSwap } from "../hooks/usePendingSwapData"
 import { commify } from "../utils"
 import { formatBNToString } from "../utils"
 import { getFormattedShortTime } from "../utils/dateTime"
-import styles from "./PendingSwapTimeline.module.scss"
 import { useTranslation } from "react-i18next"
 
 const PendingSwapTimeline = ({
@@ -33,7 +37,7 @@ const PendingSwapTimeline = ({
   const hasEvents = events.length > 0
 
   return (
-    <div className={styles.timeline}>
+    <div>
       <TimelineStep
         isActive={false}
         icon={<CheckIcon />}
@@ -144,12 +148,12 @@ const TimelineStep = ({
 }>) => {
   console.log("first", isActive)
   return (
-    <div data-testid={testId || null}>
-      {withLine ? <Line /> : null}
-      <Box color="black">{icon}</Box>
+    <TimelineItem data-testid={testId || null}>
+      <TimelineSeparator>
+        {withLine && <TimelineConnector />}
+        <TimelineDot>{icon}</TimelineDot>
+      </TimelineSeparator>
       {children}
-    </div>
+    </TimelineItem>
   )
 }
-
-const Line = () => <div className={styles.timelineLine} />
